6161f9ea900bc8e0f846469d0b23f58bebb95e73
namkungkang <namkung0131@naver.com>
2025-03-24 11:14:38 +0900

    최초 생성

---
+++ src/main/java/com/example/umcworkbook/UmcWorkbookApplication.java
@@ -0,0 +1,14 @@
+package com.example.umcworkbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcWorkbookApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Dsds");
+        SpringApplication.run(UmcWorkbookApplication.class, args);
+    }
+
+}
